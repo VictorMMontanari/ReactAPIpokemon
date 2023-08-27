88d19e57bc37aba793434f413eaeaa4dcbdc21fe
Victor M <victormm9000@gmail.com>
2023-08-27 15:01:08 -0300

    Ajuste nas tabelas

--- src/components/OtherPokemonComponent.js
+++ src/components/OtherPokemonComponent.js
@@ -1,37 +1,86 @@
 import React from "react";
 import { formatName, getTypeStyle, formatType } from "../components/format";
+import happinessImage from '../icon/happiness.png';
 
 const OtherPokemonComponent = ({ name, image, typeNames, pokemonId, evolutionImages }) => (
-    <nav className={`modulos1 ${typeNames[0]}`} key={name}>
-            <div className={`modulo1 ${typeNames[0]}`}>
-                <h4 className="th4">{formatName(name)}</h4>
-                <img className="img" src={image} alt={name} />
-                <div className="tipos">
-                    {typeNames.map((type, index) => (
-                        <span key={type} className={`tipo ${getTypeStyle(type)}`}>
-                            {formatType(type)}
-                        </span>
-                    ))}
-                </div>
-                <span className="pokeid">#{pokemonId.toString().padStart(3, '0')}</span>
-                <div className="evolutioImagesContainer">
-                    {evolutionImages.map((evolution, index) => (
-                            <div key={index} className="evolution-image">
-                                <h4>{formatName(evolution.name)}</h4>
-                                <img className="img" src={evolution.image} alt={evolution.name} />
-                                {evolution.level !== null && <p>Level: {evolution.level}</p>}
-                                {evolution.item !== null && (
-                                    <div>
-                                        <p>Item: {evolution.item.name}</p>
-                                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${evolution.item.name}.png`} alt={evolution.item.name} />
-                                    </div>
-                                )}
-                                {evolution.happiness !== null && <p>Happiness: {evolution.happiness}</p>}
-                            </div>
-                        ))}
-                </div>
+    <div className={`moduloseevee`} key={name}>
+        <div className={`moduloeevee`}>
+            <div className="t4e">
+                <h4 className="th4e">{formatName(name)}</h4>
             </div>
-        </nav>
+            <img className="img" src={image} alt={name} />
+            <div className="tipos">
+                {typeNames.map((type, index) => (
+                    <span key={type} className={`tipo ${getTypeStyle(type)}`}>
+                        {formatType(type)}
+                    </span>
+                ))}
+            </div>
+            <span className="pokeid">#{pokemonId.toString().padStart(3, '0')}</span>
+            <div className="evolutioImagesContainer">
+                <table>
+                    <tbody>
+                        <tr>
+                        {evolutionImages.map((evolution, index) => (
+                                <React.Fragment key={index}>
+                                    {evolution.level !== null && (
+                                        <td>
+                                        </td>
+                                    )}
+                                    {evolution.item !== null && (
+                                        <td>
+                                        </td>
+                                    )}
+                                    {evolution.happiness !== null && (
+                                        <td>
+                                        </td>
+                                    )}
+                                    <td className="pkmn">
+                                        <h4>{formatName(evolution.name)}</h4>
+                                    </td>
+                                </React.Fragment>
+                            ))}
+                        </tr>
+                        <tr >
+                            {evolutionImages.map((evolution, index) => (
+                                <React.Fragment key={index}>
+                                    {evolution.level !== null && (
+                                        <td className="lv">
+                                            <div className="lv-container">
+                                                <h4 className="LvPk">Lv:<br />{evolution.level}</h4>
+                                            </div>
+                                        </td>
+                                    )}
+                                    {evolution.item !== null && (
+                                        <td className="">
+                                            <div className="ev-container">
+                                                <img className="evitem"
+                                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${evolution.item.name}.png`}
+                                                    alt={evolution.item.name}
+                                                />
+                                            </div>
+                                        </td>
+                                    )}
+                                    {evolution.happiness !== null && (
+                                        <td className="">
+                                            <div className="hap-container">
+                                                <img src={happinessImage}/>
+                                                {/* <h4 className="LvPk">{evolution.happiness}</h4> */}
+                                            </div>
+                                        </td>
+                                    )}
+                                    <td className="pkmn">
+                                        {/* <h4>{formatName(evolution.name)}</h4> */}
+                                        <img className="img" src={evolution.image} alt={evolution.name} />
+                                    </td>
+                                </React.Fragment>
+                            ))}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 );
 
 export default OtherPokemonComponent;
